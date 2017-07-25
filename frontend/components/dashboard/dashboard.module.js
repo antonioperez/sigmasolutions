7c99d6ac49af6343f333836065894a4b93ab8537
danielm <danielm@mertechdata.com>
2017-07-24 19:27:13 -0700

    Updates to dashboard.

--- frontend/components/dashboard/dashboard.module.js
+++ frontend/components/dashboard/dashboard.module.js
@@ -1,27 +1,22 @@
 function config($stateProvider, $urlRouterProvider) {
     $stateProvider
 
-        // .state('index.dashboard', {
-        //     url: "/dashboard",
-        //     templateUrl: "components/dashboard/dashboard.html",
-        //     data: { pageTitle: 'Dashboard' }
-        // })
         .state('index.dashboard', {
           url: "/dashboard",
           templateUrl: "components/dashboard/dashboard.html",
           data: { pageTitle: 'Chart.js' },
           resolve: {
-              loadPlugin: function ($ocLazyLoad) {
-                  return $ocLazyLoad.load([
-                      {
-                          files: ['js/plugins/chartJs/Chart.min.js']
-                      },
-                      {
-                          name: 'angles',
-                          files: ['js/plugins/chartJs/angles.js']
-                      }
-                  ]);
-              }
+            loadPlugin: function ($ocLazyLoad) {
+              return $ocLazyLoad.load([
+                {
+                    files: ['js/plugins/chartJs/Chart.min.js']
+                },
+                {
+                    name: 'angles',
+                    files: ['js/plugins/chartJs/angles.js']
+                }
+              ]);
+            }
           }
       })
 }
