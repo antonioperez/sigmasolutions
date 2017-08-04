73ace4ac5f08c89fe3dfe96acfc2d398ab4d6df8
danielm <danielm@mertechdata.com>
2017-08-03 19:06:41 -0700

    Updated build files.

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,7 +22,7 @@ module.exports = function (grunt) {
         // The grunt server settings
         connect: {
             options: {
-                port: 8080,
+                port: 9090,
                 hostname: 'localhost',
                 livereload: 35729
             },
