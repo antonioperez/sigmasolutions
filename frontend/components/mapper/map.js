71eca7804750dec1217b306201bd8ff0e28d9900
antonioperez <aperez2541@gmail.com>
2017-08-04 19:54:33 -0700

    make map.js more lib like. Pass in zip location and background options

--- frontend/components/mapper/map.js
+++ frontend/components/mapper/map.js
@@ -1,135 +1,134 @@
 //for the shapefiles in the folder called 'files' with the name pandr.shp
 CONVERTER_URL = "http://sgma.ecoverse.io/mapper/convertJson";
 
-shp("B118_CA_GroundwaterBasins.zip").then(function (geojson) {
+var loadShapefile = function (zipPath, backgroundOptions) {
 
-    var map = L.map('map').setView([36.8, -120], 10);
-    L.esri.basemapLayer("Topographic").addTo(map);
+    shp(zipPath).then(function (geojson) {
 
-    //after map is loaded with the data. We need style it. 
-    var backgroundOverlayStyle = {
-        "color": "#A9A9A9",
-        "weight": 2,
-        "opacity": 0.65
-    };
+        var map = L.map('map').setView([36.8, -120], 10);
+        L.esri.basemapLayer("Topographic").addTo(map);
 
-    //function to display popup
-    function onEachFeature(feature, layer) {
-        var props = feature.properties;
-        var popupContent = "<p>" +
-            "Basin ID: " + props.Basin_ID + "<br>" +
-            "Basin Name: " + props.Basin_Name + "<br>" +
-            "Basin_Su_1: " + props.Basin_Su_1 + "<br>" +
-            "Region: " + props.Region_Off + "<br>" +
-            "Report: " + props.Report + "<br>" + "</p>";
+        //after map is loaded with the data. We need style it. 
+        var backgroundOverlayStyle = backgroundOptions;
 
-        if (feature.properties && feature.properties.popupContent) {
-            popupContent += feature.properties.popupContent;
-        }
-        layer.bindPopup(popupContent);
-    }
+        //function to display popup
+        function onEachFeature(feature, layer) {
+            var props = feature.properties;
+            var popupContent = "<p>" +
+                "Basin ID: " + props.Basin_ID + "<br>" +
+                "Basin Name: " + props.Basin_Name + "<br>" +
+                "Basin_Su_1: " + props.Basin_Su_1 + "<br>" +
+                "Region: " + props.Region_Off + "<br>" +
+                "Report: " + props.Report + "<br>" + "</p>";
 
-    var featuresLayer = L.geoJSON(geojson.features, {
-        style: backgroundOverlayStyle,
-        onEachFeature: onEachFeature
-    });
-
-    map.addLayer(featuresLayer);
-    //end data to map. 
-
-    //create search functions
-    var searchControl = new L.Control.Search({
-        layer: featuresLayer,
-        propertyName: 'Basin_Su_1',
-        marker: false,
-        moveToLocation: function (latlng, title, map) {
-            var zoom = map.getBoundsZoom(latlng.layer.getBounds());
-            map.setView(latlng, zoom);
-        }
-    });
-
-    searchControl.on('search:locationfound', function (e) {
-        ////if a search is found. Highlight the area
-        e.layer.setStyle({
-            fillColor: '#3f0',
-            color: '#0f0'
-        });
-        if (e.layer._popup)
-            e.layer.openPopup();
-    }).on('search:collapsed', function (e) {
-        featuresLayer.eachLayer(function (layer) {
-            //restore original background color
-            featuresLayer.resetStyle(layer);
-        });
-    });
-    map.addControl(searchControl);
-    //end search options
-
-
-    //create mapping drawing functionality
-    var drawnItems = new L.FeatureGroup();
-    map.addLayer(drawnItems);
-    var drawControl = new L.Control.Draw({
-        position: 'topright',
-        draw: {
-            polyline: true,
-            polygon: true,
-            circle: false,
-            marker: true
-        },
-        edit: {
-            featureGroup: drawnItems,
-            remove: true
-        }
-    });
-    map.addControl(drawControl);
-    //end drawing functionality
-
-    map.on(L.Draw.Event.CREATED, function (e) {
-        //custom feature is created on map. Add it or do more with it. 
-        var type = e.layerType;
-        var layer = e.layer;
-        drawnItems.addLayer(layer);
-    });
-
-    map.on(L.Draw.Event.EDITED, function (e) {
-        var layers = e.layers;
-        var countOfEditedLayers = 0;
-        layers.eachLayer(function (layer) {
-            countOfEditedLayers++;
-        });
-        console.log(e);
-        //TO DO: Need to add edited features to current geojson
-        var data = featuresLayer.toGeoJSON();
-        data = JSON.stringify(data);
-        filename = $("#js-fileName").val();
-        console.log(filename);
-
-        payload = {
-            json: data,
-            outputName: filename
-        }
-
-        //animate progress bar
-        progressBar = $("#js-map-progress-bar");
-        var percentVal = 0;
-        var interval = setInterval(function () {
-            if (percentVal <= 100) {
-                progressBar.css('width', percentVal + "%").attr('aria-valuenow', percentVal);
-                percentVal += .5;
-                percentVal = Math.round(percentVal);
-            } else if (percentVal > 100) {
-                percentVal = 0;
+            if (feature.properties && feature.properties.popupContent) {
+                popupContent += feature.properties.popupContent;
             }
-        }, 50);
-
-        var doneProcessing = function () {
-            clearInterval(interval);
-            progressBar.css('width', "0%").attr('aria-valuenow', 0);
-            console.log("test callback");
+            layer.bindPopup(popupContent);
         }
 
-        downloadFile(CONVERTER_URL, payload, doneProcessing);
-    });
+        var featuresLayer = L.geoJSON(geojson.features, {
+            style: backgroundOverlayStyle,
+            onEachFeature: onEachFeature
+        });
 
-});
+        map.addLayer(featuresLayer);
+        //end data to map. 
+
+        //create search functions
+        var searchControl = new L.Control.Search({
+            layer: featuresLayer,
+            propertyName: 'Basin_Su_1',
+            marker: false,
+            moveToLocation: function (latlng, title, map) {
+                var zoom = map.getBoundsZoom(latlng.layer.getBounds());
+                map.setView(latlng, zoom);
+            }
+        });
+
+        searchControl.on('search:locationfound', function (e) {
+            ////if a search is found. Highlight the area
+            e.layer.setStyle({
+                fillColor: '#3f0',
+                color: '#0f0'
+            });
+            if (e.layer._popup)
+                e.layer.openPopup();
+        }).on('search:collapsed', function (e) {
+            featuresLayer.eachLayer(function (layer) {
+                //restore original background color
+                featuresLayer.resetStyle(layer);
+            });
+        });
+        map.addControl(searchControl);
+        //end search options
+
+
+        //create mapping drawing functionality
+        var drawnItems = new L.FeatureGroup();
+        map.addLayer(drawnItems);
+        var drawControl = new L.Control.Draw({
+            position: 'topright',
+            draw: {
+                polyline: true,
+                polygon: true,
+                circle: false,
+                marker: true
+            },
+            edit: {
+                featureGroup: drawnItems,
+                remove: true
+            }
+        });
+        map.addControl(drawControl);
+        //end drawing functionality
+
+        map.on(L.Draw.Event.CREATED, function (e) {
+            //custom feature is created on map. Add it or do more with it. 
+            var type = e.layerType;
+            var layer = e.layer;
+            drawnItems.addLayer(layer);
+        });
+
+        map.on(L.Draw.Event.EDITED, function (e) {
+            var layers = e.layers;
+            var countOfEditedLayers = 0;
+            layers.eachLayer(function (layer) {
+                countOfEditedLayers++;
+            });
+            console.log(e);
+            //TO DO: Need to add edited features to current geojson
+            var data = featuresLayer.toGeoJSON();
+            data = JSON.stringify(data);
+            filename = $("#js-fileName").val();
+            console.log(filename);
+
+            payload = {
+                json: data,
+                outputName: filename
+            }
+
+            //animate progress bar
+            progressBar = $("#js-map-progress-bar");
+            var percentVal = 0;
+            var interval = setInterval(function () {
+                if (percentVal <= 100) {
+                    progressBar.css('width', percentVal + "%").attr('aria-valuenow', percentVal);
+                    percentVal += .5;
+                    percentVal = Math.round(percentVal);
+                } else if (percentVal > 100) {
+                    percentVal = 0;
+                }
+            }, 50);
+
+            var doneProcessing = function () {
+                clearInterval(interval);
+                progressBar.css('width', "0%").attr('aria-valuenow', 0);
+                console.log("test callback");
+            }
+
+            downloadFile(CONVERTER_URL, payload, doneProcessing);
+        });
+
+    });
+}
