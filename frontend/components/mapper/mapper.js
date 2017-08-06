80c300b3b2e025256be40151ff4d64990e146b22
antonioperez <aperez2541@gmail.com>
2017-08-06 14:50:20 -0700

    fix syntax error

--- frontend/components/mapper/mapper.js
+++ frontend/components/mapper/mapper.js
@@ -1,34 +1,32 @@
 (function () {
 
-  angular
-    .module('app')
-    .controller('MapCtrl', [
-      '$http',
-      '$scope',
-      'mapservice',
-      Ctrl
-    ]);
+    angular
+        .module('app')
+        .controller('MapCtrl', [
+            '$http',
+            '$scope',
+            'mapservice',
+            Ctrl
+        ]);
 
-  function Ctrl ($http, $scope, mapservice) {
-    var background = {
-      "weight": 2,
-      "opacity": 0.65
-    };
+    function Ctrl($http, $scope, mapservice) {
+        var background = {
+            "weight": 2,
+            "opacity": 0.65
+        };
 
-    var popupContent = "<p>" +
-      "Basin ID: {{Basin}} <br>" +
-      "GSA filing: <a href='http://sgma.water.ca.gov/portal/gsa/print/{{DWR GSA ID}}' target='_blank'>View</a> <br>" +
-      "GSA Name:  <a target='_blank' href ='{{GSA URL}} '>{{GSA Name}}  </a><br>" +
-      "POC Name: {{POC Name}} <br>" +
-      "POC Email: {{POC Email}} <br>" +
-      "POC Phone: {{POC Phone}} <br>" +
-      "Posted DT:  {{Posted DT}} <br>" + 
-      "Local ID: {{Local ID}} <br>" +"</p>";
+        var popupContent = "<p>" +
+            "Basin ID: {{Basin}} <br>" +
+            "GSA filing: <a href='http://sgma.water.ca.gov/portal/gsa/print/{{DWR GSA ID}}' target='_blank'>View</a> <br>" +
+            "GSA Name:  <a target='_blank' href ='{{GSA URL}} '>{{GSA Name}}  </a><br>" +
+            "POC Name: {{POC Name}} <br>" +
+            "POC Email: {{POC Email}} <br>" +
+            "POC Phone: {{POC Phone}} <br>" +
+            "Posted DT:  {{Posted DT}} <br>" +
+            "Local ID: {{Local ID}} <br>" + "</p>";
 
-                this.basinsmap = mapservice.generateMap("map", 8, "data/ExclusiveGsaMasterSet.zip", background,
-                    popupContent, "Basin", true
-                );
-            }
-        ])
-
+        this.basinsmap = mapservice.generateMap("map", 8, "data/ExclusiveGsaMasterSet.zip", background,
+            popupContent, "Basin", true
+        );
+    }
 })();
