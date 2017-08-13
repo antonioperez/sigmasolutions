09993f0922bd9112a5a5b6a0fa4dd997fd5608e3
antonioperez <aperez2541@gmail.com>
2017-08-13 02:41:59 -0700

    initial saving & reading files functionality

--- frontend/components/filemanager/filemanager.js
+++ frontend/components/filemanager/filemanager.js
@@ -11,29 +11,46 @@
 
     function Ctrl($http, $scope, FileUploader) {
 
-        var auth = firebase.auth();
-        var storageRef = firebase.storage().ref();
 
         var uploader = $scope.uploader = new FileUploader();
+        var auth = firebase.auth();
+        var storageRef = firebase.storage().ref();
+        var database = firebase.database();
+        var recentFilesRef = database.ref('uploads/userid').limitToLast(10);
+        $scope.uploadedFiles = [];
+
+
+        recentFilesRef.on('child_added', function(data) {
+                var childData = data.val();
+                $scope.uploadedFiles.push(childData);
+                $scope.$apply();
+        });
+
+        function writeUserData(userId, filename, size, downloadUrl, lastModified) {
+            //fancy hashing algorithm goes here
+            var encodedData = window.btoa(filename+size);
+            var newRef = firebase.database().ref('uploads/' + userId).child(encodedData);
+            newRef.set({
+                name: filename,
+                size: size,
+                downloadUrl: downloadUrl,
+                lastModified: lastModified
+            });
+        }
 
         uploader.uploadItem = function (value) {
 
             //HAD TO OVERWRITE EXISTING UPLOAD ITEM FUNCTION. 
             //BECAUSE IT IS SENDING TO A LOCAL PORT/URL. NEED TO SEND TO FIREBASE INSTEAD
 
-            var file = value._file;           
+            var file = value._file;
             storageRef.child('userid/' + file.name).put(file).then(function (snapshot) {
-                console.log('Uploaded a blob or file!');
+                var downloadURL = snapshot.downloadURL;
+                writeUserData("userid", file.name, file.size, downloadURL, file.lastModified);
 
             }, function (error) {
                 // Handle unsuccessful uploads
                 console.log(error);
-            }, function () {
-
-                // Handle successful uploads on complete
-                // For instance, get the download URL: https://firebasestorage.googleapis.com/...
-                var downloadURL = uploadTask.snapshot.downloadURL;
-                console.log(downloadURL);
             });
 
             var index = this.getIndexOfItem(value);
@@ -41,7 +58,7 @@
             var transport = this.isHTML5 ? '_xhrTransport' : '_iframeTransport';
 
             item._prepareToUploading();
-            if(this.isUploading) return;
+            if (this.isUploading) return;
 
             this._onBeforeUploadItem(item);
             if (item.isCancel) return;
@@ -52,38 +69,9 @@
             this._render();
         };
 
-
-
-        // // CALLBACKS
-        // uploader.onWhenAddingFileFailed = function (item /*{File|FileLikeObject}*/ , filter, options) {
-        //     console.info('onWhenAddingFileFailed', item, filter, options);
-        // };
-
-        // uploader.onAfterAddingAll = function (addedFileItems) {
-        //     console.info('onAfterAddingAll', addedFileItems);
-        // };
-
-        // uploader.onProgressItem = function (fileItem, progress) {
-        //     console.info('onProgressItem', fileItem, progress);
-        // };
-        // uploader.onProgressAll = function (progress) {
-        //     console.info('onProgressAll', progress);
-        // };
-        // uploader.onSuccessItem = function (fileItem, response, status, headers) {
-        //     console.info('onSuccessItem', fileItem, response, status, headers);
-        // };
-        // uploader.onErrorItem = function (fileItem, response, status, headers) {
-
-        // };
-        // uploader.onCancelItem = function (fileItem, response, status, headers) {
-        //     console.info('onCancelItem', fileItem, response, status, headers);
-        // };
-        // uploader.onCompleteItem = function (fileItem, response, status, headers) {
-        //     console.info('onCompleteItem', fileItem, response, status, headers);
-        // };
-        // uploader.onCompleteAll = function () {
-        //     console.info('onCompleteAll');
-        // };
+        uploader.onCancelItem = function (fileItem, response, status, headers) {
+            console.info('onCancelItem', fileItem, response, status, headers);
+        };
 
     }
 })();
