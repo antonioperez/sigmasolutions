8660c660674ceda26464c4edab798caa41f1d77d
antonioperez <aperez2541@gmail.com>
2017-08-14 18:14:39 -0700

    initial gsa section 1 form

--- frontend/components/gsa/gsa.js
+++ frontend/components/gsa/gsa.js
@@ -3,29 +3,74 @@
   angular
     .module('app')
     .controller('GSACtrl', Ctrl);
-    
-    function Ctrl () {
-      var vm = this;
 
-      vm.faqIndex = 0;
-      vm.questionIndex = 0;
+  function Ctrl() {
+    var vm = this;
 
-      vm.questions = [
-        {
-          title: 'Decision To Become A GSA',
-          info: 'Please enter the date your local agency, or agencies, decided to become or form a GSA.',
-          template: function () {
-            return 'components/gsa/decision-to-become-gsa.html';
-          }
-        }
-      ];
-
-      vm.faqs = [
+    vm.questionIndex = 0;
+    vm.questions = [{
+        title: 'Becoming a GSA',
+        info: 'What is your GSAs name?',
+        template: function () {
+          return 'components/gsa/decision-to-become-gsa.html';
+        },
+        faqs: [
         {
           question: 'What is water code 10721?',
           answer: '"Local agency" means a local public agency that has water supply, water management, or land use responsibilities within a groundwater basin.'
+        },  
+        {
+          question: 'What is water code 10723?',
+          answer: 'Any local agency or combination of local agencies overlying a groundwater basin may decide to become a GSA for that basin.'
+        }]
+      },
+      {
+        title: 'Becoming a GSA',
+        info: 'Are you an exclusive agency?',
+        template: function () {
+          return 'components/gsa/decision-to-become-gsa.html';
+        },
+        faqs: [],
+        moreAction: {
+          title: 'Information About GSA Eligibility',
+          info: 'As discussed in this document, formation of a GSA is not necessary if a local agency plans to submit an Alternative Plan for an entire basin by January 1, 2017. Additional information about GSAs and the requirement to develop groundwater sustainability plans (GSPs) by 2020 or 2022, or Alternative Plans by 2017, is available on DWR’s Sustainable Groundwater Management website included here: http://water.ca.gov/groundwater/sgm/index.cfm',
+          template: function () {
+            return 'components/gsa/decision-to-become-gsa.html';
+          },
+          faqs: []
         }
-      ];
-    }
+      },{
+        title: 'Becoming a GSA',
+        info: 'Are you going to form a GSA with multiple local agencies?',
+        template: function () {
+          return 'components/gsa/decision-to-become-gsa.html';
+        },
+        faqs: [],
+        moreAction: {
+          info: 'Upload joint Powers Agreeement, Memorandum of Agreement and Coordination Agreements',
+          template: function () {
+            return 'components/gsa/decision-to-become-gsa.html';
+          },
+          faqs: [
+            {
+              question: 'What are these forms?',
+              answer: 'SGMA allows multiple local agencies to act as a single GSA through a memorandum of agreement (MOA), a joint powers agreement (JPA), or any other legal agreement (California Water Code, Section [§] 10723.6'
+            }
+          ]
+        }
+      },
+      {
+        title: 'Becoming a GSA',
+        info: 'Attach statutary area boundary shapefile',
+        template: function () {
+          return 'components/gsa/decision-to-become-gsa.html';
+        },
+        faqs: [{
+          question: 'Shapefiles Guidelines',
+          answer: '"A service area shapefile attribute table should contain one record in the attribute table per service area. Include at least one field in the attribute table that identifies the name of the local agency.'
+        }]
+      }
+    ];
+  }
 
 })();
