74172604e319f750020b70a585602a9938ded77a
antonioperez <aperez2541@gmail.com>
2017-08-14 20:15:25 -0700

    merge

--- frontend/components/gsa/gsa.js
+++ frontend/components/gsa/gsa.js
@@ -8,57 +8,73 @@
     var vm = this;
 
     vm.options = ['first option', 'second option', 'third option'];
+    vm.answers = {
 
+    };
     vm.sectionIndex = 0;
     vm.questionIndex = 0;
+    vm.activeSection = {};
+    vm.activeQuestion = {};
+
     vm.questions = [{
+        id: "1.0",
         info: 'You have determined that you are either an existing local agency eligible to become a GSA, or have formed a GSA through a legal agreement with other GSA-eligible local agencies.',
+        questionKey: "Agency Name",
+        value: "",
         template: function () {
           return 'components/gsa/1.0.html';
         },
-        faqs: [
-        {
-          question: 'What is water code 10721?',
-          answer: '"Local agency" means a local public agency that has water supply, water management, or land use responsibilities within a groundwater basin.'
-        },  
-        {
-          question: 'What is water code 10723?',
-          answer: 'Any local agency or combination of local agencies overlying a groundwater basin may decide to become a GSA for that basin.'
-        }]
+        faqs: [{
+            question: 'What is water code 10721?',
+            answer: '"Local agency" means a local public agency that has water supply, water management, or land use responsibilities within a groundwater basin.'
+          },
+          {
+            question: 'What is water code 10723?',
+            answer: 'Any local agency or combination of local agencies overlying a groundwater basin may decide to become a GSA for that basin.'
+          }
+        ]
       },
       {
-        info: 'Are you an exclusive agency?',
+        id: "1.1",
+        info: 'These local agencies will be considered "exclusive" within their statutory boundaries unless they opt out of this responsibility by sending a notice to DWR. No other local agency operating within the statutory boundaries of these exclusive local agencies may decide to become a GSA in these areas until DWR has received a notice stating otherwise.',
+        questionKey: "Exclusive Agency",
         template: function () {
           return 'components/gsa/1.1.html';
         },
-        faqs: [],
+        faqs: [{
+          question: 'What is an exclusive agency?',
+          answer: 'Only exclusive GSAs can coordinate to develop a GSP for a basin and submit that GSP to DWR for review. If not, there are alternative plans.'
+        }],
         moreAction: {
-          info: 'As discussed in this document, formation of a GSA is not necessary if a local agency plans to submit an Alternative Plan for an entire basin by January 1, 2017. Additional information about GSAs and the requirement to develop groundwater sustainability plans (GSPs) by 2020 or 2022, or Alternative Plans by 2017, is available on DWR’s Sustainable Groundwater Management website included here: http://water.ca.gov/groundwater/sgm/index.cfm',
+          id: "1.1.0",
+          questionKey: "Alternative Plan",
+          info: 'Formation of a GSA is not necessary if a local agency plans to submit an Alternative Plan for an entire basin by January 1, 2017. Additional information about GSAs and the requirement to develop groundwater sustainability plans (GSPs) by 2020 or 2022, or Alternative Plans by 2017, is available on DWR’s Sustainable Groundwater Management website included here: http://water.ca.gov/groundwater/sgm/index.cfm',
           template: function () {
-            return 'components/gsa/decision-to-become-gsa.html';
+            return 'components/gsa/1.1.0.html';
           },
           faqs: []
         }
-      },{
+      }, {
+        questionKey: "Multiple Agencies",
         info: 'Are you going to form a GSA with multiple local agencies?',
         template: function () {
-          return 'components/gsa/decision-to-become-gsa.html';
+          return 'components/gsa/1.2.html';
         },
         faqs: [],
         moreAction: {
+          questionKey: "Joint Powers",
           info: 'Upload joint Powers Agreeement, Memorandum of Agreement and Coordination Agreements',
           template: function () {
             return 'components/gsa/decision-to-become-gsa.html';
           },
-          faqs: [
-            {
-              question: 'What are these forms?',
-              answer: 'SGMA allows multiple local agencies to act as a single GSA through a memorandum of agreement (MOA), a joint powers agreement (JPA), or any other legal agreement (California Water Code, Section [§] 10723.6'
-            }
-          ]
+          faqs: [{
+            question: 'What are these forms?',
+            answer: 'SGMA allows multiple local agencies to act as a single GSA through a memorandum of agreement (MOA), a joint powers agreement (JPA), or any other legal agreement (California Water Code, Section [§] 10723.6'
+          }]
         }
       },
       {
+        questionKey: "Boundary",
         info: 'Attach statutary area boundary shapefile',
         template: function () {
           return 'components/gsa/decision-to-become-gsa.html';
@@ -70,12 +86,37 @@
       }
     ];
 
-    vm.sections = [
-      {
-        title : "Becoming a GSA",
-        questions : vm.questions
+    vm.sections = [{
+      title: "Becoming a GSA",
+      questions: vm.questions
+    }]
+
+    vm.fillAnswer = function () {
+      var answer = vm.activeSection.questions[vm.questionIndex].value;
+      var questionKey = vm.activeSection.questions[vm.questionIndex].questionKey;
+      if (answer) {
+        vm.answers[questionKey] = answer;
       }
-    ]
+    }
+
+    vm.nextQuestion = function () {
+      vm.fillAnswer();
+      vm.questionIndex += 1;
+    }
+
+    vm.moreAction = function () {
+      vm.fillAnswer();
+      vm.activeSection.questions[vm.questionIndex] = vm.activeSection.questions[vm.questionIndex].moreAction;
+    }
+
+    vm.showAnswers = function () {
+      console.log(vm.answers);
+    }
+
+    vm.init = function () {
+      vm.activeSection = vm.sections[vm.sectionIndex];
+    }
+    vm.init();
   }
 
 })();
