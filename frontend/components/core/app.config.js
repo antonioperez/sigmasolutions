6aedaf69d1dfccdeec856e97d4851026ba5e64e1
daniel-macleod <dj.macleod89@gmail.com>
2017-08-16 21:42:01 -0700

    Add logout functionality and check for valid session.

--- frontend/components/core/app.config.js
+++ frontend/components/core/app.config.js
@@ -5,7 +5,8 @@
     .config([ '$stateProvider', '$urlRouterProvider', config ])
     .run([ '$rootScope', '$state', function($rootScope, $state) {
       $rootScope.$state = $state;
-    }]);
+    }])
+    .run(['$rootScope', '$state', statesConfig]);
 
   function config($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.otherwise("/login");
@@ -15,6 +16,17 @@
         url: "/index",
         templateUrl: "components/layout/content.html",
       })
+  }
+
+  function statesConfig ($rootScope, $state) {
+    $rootScope.$on('$stateChangeStart', function (evt, toState, toParams) {
+      if (!firebase.auth().currentUser) {
+        if (toState.name !== 'login') {
+          evt.preventDefault();
+          $state.go('login');
+        }
+      } 
+    });
   }
 
 })();
